172eac6acbe62dfb290a0479cbb2b9c66c10c881
Ryan Blessing <ryanblessing20@yahoo.com>
2021-05-25 23:44:23 -0500

    updated id's and varables starting

--- assets/js/JS.js
+++ assets/js/JS.js
@@ -1,28 +1,8 @@
-
-
-var currentTime
-var cityName = $("#city-search").Val().trim();
-var APIkey = "714e6c9a22msh451a05fe981a121p1a08abjsn5b1fece89a2f";
-var queryUrl = "https://community-open-weather-map.p.rapidapi.com/forecast?";
-
-
-function weatherForecast()
-$("")
-
-
-
-/*
-fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=cityname", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "714e6c9a22msh451a05fe981a121p1a08abjsn5b1fece89a2f",
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-*/
+//declared varibles to start off my functions
+var citySearch = $("#searchEl");
+var searchBtn = $("#search-btn");
+var currentCity = $("#now-city");
+var temperature = $("#temperature");
+var humidity = $("#humidity");
+var windSpeed = $("wind-speed");
+var uvIndex = $("#uv-index");
