6f8760c1d58f5a64d53e13a0590028662d96ab4b
Ryan Blessing <ryanblessing20@yahoo.com>
2021-05-25 23:32:16 -0500

    updated html to help me format the JS

---
+++ assets/js/JS.js
@@ -0,0 +1,28 @@
+
+
+var currentTime
+var cityName = $("#city-search").Val().trim();
+var APIkey = "714e6c9a22msh451a05fe981a121p1a08abjsn5b1fece89a2f";
+var queryUrl = "https://community-open-weather-map.p.rapidapi.com/forecast?";
+
+
+function weatherForecast()
+$("")
+
+
+
+/*
+fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=cityname", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "714e6c9a22msh451a05fe981a121p1a08abjsn5b1fece89a2f",
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+*/
--- assets/js/moment/moment.js
+++
@@ -1,5 +0,0 @@
-//displaying current time for city
-var time = moment();
-var displayMoment = document.getElementById("current-time");
-displayMoment.innerHTML = time.format("MMMM Do YYYY, h:m a");
-var nowTime = moment().hours()
